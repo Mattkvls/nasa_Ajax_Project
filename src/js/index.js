39634766caeb7bd93d4a10729c300e573820c749
Mattkvls <matthew.katsivelis@gmail.com>
2019-11-22 16:49:35 +0200

    create the model to get the picture of the day, save it to the newly created object

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,7 @@
-import axios from 'axios';
-async function getImageOfDay(){
-    const key= CbSMLxCRncwtSTBVz87TluM0lnbX9Sd9tQY3ja5U;
-    const result =await axios(`https://api.nasa.gov/planetary/apod?api_key=${key}`);
-    console.log(result);
-}
-getImageOfDay();
+import Picture from './models/PictureOfDay';
 
-//https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
-// CbSMLxCRncwtSTBVz87TluM0lnbX9Sd9tQY3ja5U
-
-
-
+//the object i created for the Picture has the getImageOfDay method
+const picture= new Picture();
+console.log(picture);
+picture.getImageOfDay();
 
---
+++ src/js/models/PictureOfDay.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+//describe data model for the pictureOfDay
+//crete a new object with the results of the ajax call
+export default class Picture{
+    constructor(){}
+
+    // async method of this class
+    async getImageOfDay(){
+        try{
+            const res=await axios(`https://api.nasa.gov/planetary/apod?api_key=CbSMLxCRncwtSTBVz87TluM0lnbX9Sd9tQY3ja5U`);
+
+        //api_key=CbSMLxCRncwtSTBVz87TluM0lnbX9Sd9tQY3ja5U
+        //save the data to the object as result
+        this.result=res.data;
+        console.log(res.data);
+        
+        }catch(error){
+            alert(error);
+        }     
+    }
+}
