e60509de246c9c4dba6f03be91e1624e52e71f12
Mattkvls <matthew.katsivelis@gmail.com>
2019-11-22 18:49:14 +0200

    create the controller for the picture of the day

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,24 @@
 import Picture from './models/PictureOfDay';
 
-//the object i created for the Picture has the getImageOfDay method
-const picture= new Picture();
-console.log(picture);
-picture.getImageOfDay();
+/*Global state of the app 
+*-picture object
+*-asteroids object
+*-marsWeather object
+*-Search object images
+*/
+const state={};
+
+const  controlPicture=async ()=>{
+    //create the picture object in the state
+    state.picture=new Picture();
+
+    //get the image of the day  save it to the state
+    await state.picture.getImageOfDay();
+    
+
+    //render the image of the day at the UI
+    console.log(state.picture.result);
+}
+controlPicture();
+
 
